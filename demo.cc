7706e17569aed8ede9009512a907c848058c8d71
alicxo <alicxo@163.com>
2020-06-28 23:54:41 +0800

    add function f

--- demo.cc
+++ demo.cc
@@ -1,6 +1,11 @@
 #include<iostream>
 
+void f() {
+  std::cout << "I am f" << std::endl;
+}
+
 int main(int argc, char **argv) {
+  f();
   std::cout << "hello_world" << std::endl;
   std::cout << "end" << std::endl;
   return 0;
